b1c1b1ebcfbc0dd8a19df1a8c42c7f38285546fe
Mihai <mihaimanea0306@gmail.com>
2026-04-07 10:30:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Iniciando sistema de Mihai...");
+
+// Un pequeño efecto de bienvenida
+window.onload = () => {
+    console.log("%c xA Addon Loaded ", "background: #a333ff; color: #fff; font-weight: bold;");
+};
+
